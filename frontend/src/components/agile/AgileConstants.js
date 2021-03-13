4faa7ece2fd764e69a4be1069155041160f9dc01
Yuriko <ytamura@gmail.com>
2021-03-13 12:16:16 -0800

    feat(agile): status addition and icon updates

--- frontend/src/components/agile/AgileConstants.js
+++ frontend/src/components/agile/AgileConstants.js
@@ -1,15 +1,19 @@
 export const statusMap = {
   'IN_WISHLIST': {'icon': 'gift', 'color': 'purple'},
-  'BACKLOG': {'icon': 'hourglass start', 'color': 'blue'},
+  'BACKLOG': {'icon': 'hourglass start', 'color': 'violet'},
+  'PAUSED': {'icon': 'pause circle', 'color': 'blue'},
   'IN_PROGRESS': {'icon': 'game', 'color': 'orange'},
   'COMPLETED': {'icon': 'check circle', 'color': 'green'},
-  'WONT_FINISH': {'icon': 'dont', 'color': 'red'},
+  'WONT_FINISH': {'icon': 'stop circle', 'color': 'red'},
 };
 
 export const statusGroups = [
-  {'heading': 'In Progress', 'statuses': ['IN_PROGRESS']},
-  {'heading': 'Backlog & Wishlist', 'statuses': ['BACKLOG', 'IN_WISHLIST']},
-  {'heading': 'Done for Now', 'statuses': ['COMPLETED', 'WONT_FINISH']},
+  {'heading': 'In Progress',
+   'statuses': ['IN_PROGRESS']},
+  {'heading': 'Backlog & Wishlist',
+   'statuses': ['BACKLOG', 'IN_WISHLIST', 'PAUSED']},
+  {'heading': 'Done for Now',
+   'statuses': ['COMPLETED', 'WONT_FINISH']},
 ];
 
 export const allPlayers = [ 'Fordon', 'Fumo' ];
